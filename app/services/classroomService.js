bacd47b3a113c17ff697dbd471cf2ffc086a7973
Luiz Eduardo Costa <costaeduardoluiz@gmail.com>
2018-09-23 22:27:02 -0300

    Added classes to manage the services of CRUD for Students and Classrooms.

---
+++ app/services/classroomService.js
@@ -0,0 +1,40 @@
+var classroomService = (function () {
+
+    // Local Storage's name.
+    var _localStorageKey = 'classrooms';
+    // Initialization of the list.
+    var _classrooms = [];
+
+    _loadClassrooms();
+
+    // Loads all the classrooms from localstorage.
+    function _loadClassrooms() {
+        var str = window.localStorage[_localStorageKey] || '[]';
+        _classrooms = JSON.parse(str);
+    }
+
+    // It returns the list of classrooms.
+    function getClassrooms() {
+        return _classrooms;
+    }
+
+    // Add a new classroom.
+    function addClassrooms(classroom) {
+        _classrooms.push(classroom);
+        window.localStorage[_localStorageKey] = JSON.stringify(_classrooms);
+    }
+
+    // Update a classroom.
+    function updateClassroom(classroom, index) {
+        _classrooms[index] = classroom;
+        window.localStorage[_localStorageKey] = JSON.stringify(_classrooms);
+    }
+
+    // Public methods.
+    return {
+        getClassrooms: getClassrooms,
+        addClassrooms: addClassrooms,
+        updateClassroom: updateClassroom
+    };
+
+})();
---
+++ app/services/studentService.js
@@ -0,0 +1,40 @@
+var studentService = (function () {
+
+    // Local Storage's name.
+    var _localStorageKey = 'students';
+    // Initialization of the list.
+    var _students = [];
+
+    _loadStudents();
+
+    // Loads all the students from localstorage.
+    function _loadStudents() {
+        var str = window.localStorage[_localStorageKey] || '[]';
+        _students = JSON.parse(str);
+    }
+
+    // It returns the list of students.
+    function getStudents() {
+        return _students;
+    }
+
+    // Add a new student.
+    function addStudent(student) {
+        _students.push(student);
+        window.localStorage[_localStorageKey] = JSON.stringify(_students);
+    }
+
+    // Update a student.
+    function updateStudent(student, index) {
+        _students[index] = student;
+        window.localStorage[_localStorageKey] = JSON.stringify(_students);
+    }
+
+    // Public methods.
+    return {
+        getStudents: getStudents,
+        addStudent: addStudent,
+        updateStudent: updateStudent
+    };
+
+})();
