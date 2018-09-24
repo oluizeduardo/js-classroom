1b9c54603f77aeb4864f18202976fd6e7e2c2086
Luiz Eduardo Costa <costaeduardoluiz@gmail.com>
2018-09-24 18:13:56 -0300

    Updated server.js to load service files.

--- app/server.js
+++ app/server.js
@@ -7,6 +7,7 @@ require('./routes/rt_classroom')(_app);
 require('./routes/rt_home')(_app);
 
 _app.use('/controllers', expressStatic('controllers'));
+_app.use('/services', expressStatic('services'));
 
 _app.listen(_port, function(){
   console.log("js-classroom server is running at "+_port);
