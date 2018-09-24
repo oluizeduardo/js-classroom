78797a1350e32d85596a2dc7dc04138379751e42
Luiz Eduardo Costa <costaeduardoluiz@gmail.com>
2018-09-24 17:57:41 -0300

    Updated server.js to load static contents.

--- app/server.js
+++ app/server.js
@@ -1,8 +1,12 @@
-var _app = require('./config/express')();
-var _routStudents = require('./routes/rt_students')(_app);
-var _routClassroom = require('./routes/rt_classroom')(_app);
-var _routHome = require('./routes/rt_home')(_app);
+var express = require('./config/express');
+var expressStatic = require('express-static');
+var _app = express();
 var _port = 3000;
+require('./routes/rt_students')(_app);
+require('./routes/rt_classroom')(_app);
+require('./routes/rt_home')(_app);
+
+_app.use('/controllers', expressStatic('controllers'));
 
 _app.listen(_port, function(){
   console.log("js-classroom server is running at "+_port);
